009a8d889f284204a79c9b7484309138f6bbb1b0
BeyondConfused <114650102+BeyondConfused@users.noreply.github.com>
2023-02-09 20:15:02 +0100

    Updated program to work with display and light properly

--- main.ino
+++ main.ino
@@ -1,73 +1,77 @@
 
 #include <LiquidCrystal_I2C.h>
-
 // Libraries for temperature
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-
-#define BUTTON_PIN 4
-
-
 // Libraries for temperature
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
-const int SENSOR_PIN = 4; // Temperatur data pin
+// Defines the temperatur sensor pin to get data
+const int SENSOR_PIN = 2; 
 
 OneWire oneWire(SENSOR_PIN);         
 DallasTemperature tempSensor(&oneWire);
 
-float tempCelsius;    // temperature in Celsius
-
-
+// Temperature in Celsius
+float tempC;    
+int LDRvalue = 0;
 
 // Include the Arduino Stepper Library
 #include <Stepper.h>
+// Readies the LCD display at the 0x27 address
 LiquidCrystal_I2C lcd(0x27, 16, 4);
 
-int counter = 0;
-int mad = 210;
+#define LDRpin A3
 
 
 
 void setup()
-{
-  tempSensor.begin();    // initialize the sensor
-  // set the speed at 60 rpm:
-  // initialize the serial port:
+{ // Starts the sensor
+  tempSensor.begin();    
+  // Starts serial port used for debugging
   Serial.begin(9600);
-
-     lcd.init();
-
-
+  // Starts LCD display
+  lcd.init();
   lcd.backlight();
+lcd.setCursor(0,0); 
+lcd.print("Current status:");
+lcd.setCursor(0,1);
+lcd.print("Tmp: ");
+lcd.setCursor(6,1);
+lcd.print("°C");
+lcd.setCursor(0,2);
+lcd.print("Light:");
+lcd.setCursor(0,3);
+lcd.print("Water level: ");
 
 
-  pinMode(BUTTON_PIN,INPUT_PULLUP);
-  
+
 }
 
 void loop() 
 {
-tempSensor.requestTemperatures();             // send the command to get temperatures
-  tempCelsius = tempSensor.getTempCByIndex(0);  // read temperature in Celsius
-
-
+// Reads the current light level near the LDR and saves it to variable
+  LDRvalue = analogRead(LDRpin);
+  // LDR print for debugging
+  Serial.println(LDRvalue);
+  
+//Requests the current temperature from the sensor
+tempSensor.requestTemperatures();   
+// Saves the current temperature to a variable          
+  tempC = tempSensor.getTempCByIndex(0);  
+// Prints for debugging
   Serial.print("Temperature: ");
-  Serial.print(tempCelsius);    // print the temperature in Celsius
+  Serial.print(tempC);    
   Serial.println("°C");
 
-lcd.setCursor(0,0);
-lcd.print("Placeholder1");
-lcd.setCursor(0,1);
-lcd.print("placeholder2");
-lcd.setCursor(0,2);
-lcd.print("placeholder3");
+lcd.setCursor(5,1);
+lcd.print(tempC,1) // Comma might be redundant;
+lcd.setCursor(7,2);
+lcd.print(LDRvalue);
 lcd.setCursor(0,3);
-lcd.print("placeholder4");
-
-
-  
+lcd.print("NOPE");
+// If this fails use lcd.clear between updates
+// Delay between status checks
+delay(100);
 
 }
